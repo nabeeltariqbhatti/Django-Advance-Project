1d29bcf29aa6ae48d7a8f8e758b2727f6bc23e02
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-03-30 16:26:36 +0500

    models nearly completed with relationships

--- src/sales/models.py
+++ src/sales/models.py
@@ -3,36 +3,35 @@ from products.models import Product
 from customers.models import Customer
 from profiles.models import Profile
 from django.utils import timezone
+from django.shortcuts import reverse
 
 
 class Position(models.Model):
-    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.FloatField(blank=True)
     created_at = models.DateTimeField(blank=True)
 
     def save(self, *args, **kwargs):
-        self.price = self.product * self.quantity
+        self.price = self.product.price * self.quantity
         return super().save(*args, **kwargs)
 
+    def get_sales_id(self):
+        sale_obj = self.sale_set.first()
+        return sale_obj.id
+
+    def get_sales_customer(self):
+        sale_obj = self.sale_set.first()
+        return sale_obj.customer.name
+
     def __str__(self):
-        return f"id:{self.id}, product:{self.product.name}, quantity:{self.quantity}"
-
-    def save(self, *args, **kwargs):
-        if self.transavtion_id == "":
-            self.transaction_id = ''
-        if self.created_at is None:
-            self.created_at = timezone.now()
-            return super().save(*args, **kwargs)
-
-    def get_positions(self):
-        return self.postions.all()
+        return f"id: {self.id}, product: {self.product.name}, quantity: {self.quantity}"
 
 
 class Sale(models.Model):
     transaction_id = models.CharField(max_length=12, blank=True)
     positions = models.ManyToManyField(Position)
-    total_price = models.FloatField(blank=True)
+    total_price = models.FloatField(blank=True, null=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     salesman = models.ForeignKey(Profile, on_delete=models.CASCADE)
     created_at = models.DateTimeField(blank=True)
@@ -41,12 +40,25 @@ class Sale(models.Model):
     def __str__(self):
         return f"Sales for the amount of ${self.total_price}"
 
+    def get_absolute_url(self):
+        return reverse('sales:detail', kwargs={'pk': self.pk})
+
+    def save(self, *args, **kwargs):
+        if self.transaction_id == "":
+            self.transaction_id = ''
+        if self.created_at is None:
+            self.created_at = timezone.now()
+        return super().save(*args, **kwargs)
+
+    def get_positions(self):
+        return self.positions.all()
+
 
 class CSV(models.Model):
-    file_name=models.FileField(upload_to='csvs')
-    activated=models.BooleanField(default=False)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    file_name = models.CharField(max_length=120, null=True)
+    csv_file = models.FileField(upload_to='csvs', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.file_name)
